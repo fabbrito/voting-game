48e5fe4d44de2adc24f71c7fd858f8daf8f01916
Fabrício <fab.brito94@gmail.com>
2022-07-15 02:38:36 -0300

    Add tRPC

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,9 @@
+import * as trpcNext from "@trpc/server/adapters/next";
+import { appRouter } from "@/backend/router/index";
+import type { AppRouter } from "@/backend/router/index";
+
+// export API handler
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => null,
+});
