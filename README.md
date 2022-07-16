cd60a9c1e5cc03e8d51640b618d0795b1a06f725
Fabrício <fab.brito94@gmail.com>
2022-07-16 17:25:29 -0300

    Change env MY_VERCEL_URL

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 
 [Theo - ping․gg >>> Building a MODERN Full Stack App for FREE w/ NextJS 12, Typescript, Prisma and more](https://www.youtube.com/watch?v=PKy2lYEnhgs)
 
-- [Timestamp 28:12](https://youtu.be/PKy2lYEnhgs?t=1692)
+- [Timestamp 49:54](https://youtu.be/PKy2lYEnhgs?t=2994)
+
 ### Not using:
 
 [Tailwind just-in-time mode](https://v2.tailwindcss.com/docs/just-in-time-mode)
--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -3,14 +3,15 @@ import { z } from "zod";
 import { MAX_POKEDEX_ID } from "@/utils/getRandomPokemon";
 import { PokemonClient } from "pokenode-ts";
 
-export const appRouter = trpc.router().query("get-pokemon-by-id", {
-  input: z.object({ id: z.number().min(1).max(MAX_POKEDEX_ID) }),
-  async resolve({ input }) {
-    const api = new PokemonClient();
-    const pokemon = await api.getPokemonById(input.id);
-    return {name: pokemon.name, sprites: pokemon.sprites};
-  },
-});
+export const appRouter = trpc.router()
+  .query("get-pokemon-by-id", {
+    input: z.object({ id: z.number().min(1).max(MAX_POKEDEX_ID) }),
+    async resolve({ input }) {
+      const api = new PokemonClient();
+      const pokemon = await api.getPokemonById(input.id);
+      return { name: pokemon.name, sprites: pokemon.sprites };
+    },
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
