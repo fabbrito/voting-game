51f33032029f01a396b2cf5f6e814e7b9c5167f3
Fabrício <fab.brito94@gmail.com>
2022-07-17 05:46:20 -0300

    Basic UI using R6 ops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,43 @@
-import { getOptionsForVote } from "@/utils/getRandomPokemon";
-import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
-import Image from "next/image";
+// import Image from "next/image";
 import { useEffect, useState } from "react";
+import { trpc } from "@/utils/trpc";
+
+import getOptionsForVote from "@/utils/getOptionsForVote";
+// import { MAX_POKEDEX_ID } from "@/utils/pokemonUtils";
+import { MAX_R6OPERATORS_ID } from "@/utils/r6OperatorsUtils";
 
 const Home: NextPage = () => {
-  const [ids, setIds] = useState<number[]>(getOptionsForVote());
+  const [ids, setIds] = useState<number[]>(getOptionsForVote(MAX_R6OPERATORS_ID));
   const [firstId, secondId] = ids;
 
-  const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: firstId }]);
-  const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: secondId }]);
+  // const first = trpc.useQuery(["get-pokemon-by-id", { id: firstId }]);
+  // const second = trpc.useQuery(["get-pokemon-by-id", { id: secondId }]);
 
-  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+  const first = trpc.useQuery(["get-r6-operator-by-id", { id: firstId }]);
+  const second = trpc.useQuery(["get-r6-operator-by-id", { id: secondId }]);
+
+  if (first.isLoading || second.isLoading) return null;
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which do you preffer to play as?</div>
       <div className="p-2" />
       <div className="max-w-2xl border rounded p-8 flex justify-between items-center">
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-48 h-full flex flex-col">
           <picture>
-            <img
-              src={firstPokemon.data?.sprites.front_default ?? ""}
-              alt={firstPokemon.data?.name}
-              className="w-full"
-            />
+            {/* <img src={first.data?.sprites.front_default ?? ""} alt={first.data?.name} className="w-full" /> */}
+            <img src={`data:image/svg+xml;utf8,${first.data?.svgURIencoded}`} alt={first.data?.name} />
           </picture>
-          <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
+          <div className="text-xl text-center capitalize">{first.data?.name}</div>
         </div>
-        <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col">
-          <picture className="w-full">
-            <img
-              src={secondPokemon.data?.sprites.front_default ?? ""}
-              alt={secondPokemon.data?.name}
-              className="w-full"
-            />
+        <div className="p-4">Vs</div>
+        <div className="w-48 h-full flex flex-col">
+          <picture>
+            {/* <img src={second.data?.sprites.front_default ?? ""} alt={second.data?.name} className="w-full" /> */}
+            <img src={`data:image/svg+xml;utf8,${second.data?.svgURIencoded}`} alt={second.data?.name} />
           </picture>
-          <div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
+          <div className="text-xl text-center capitalize">{second.data?.name}</div>
         </div>
       </div>
     </div>
