ea9f513ad0dd4cf4f991fdec615e50a5e4fe3f24
Fabrício <fab.brito94@gmail.com>
2022-07-17 05:44:51 -0300

    Utils for r6 operators

---
+++ src/utils/r6OperatorsUtils.ts
@@ -0,0 +1,7 @@
+import r6Operators from "r6operators";
+import type { Operator } from "r6operators";
+
+// export type TOperator = keyof typeof r6Operators;
+// export const r6OperatorsList = Object.keys(r6Operators) as Array<TOperator>;
+
+export const MAX_R6OPERATORS_ID = 69;
