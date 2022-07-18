2a4f01b792c36f8dae124f6663a1a207719e91f0
Fabrício <fab.brito94@gmail.com>
2022-07-18 05:43:51 -0300

    Add helper to infer types from queries in trps

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,9 +1,15 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import { appRouter } from "@/backend/router/index";
 import type { AppRouter } from "@/backend/router/index";
+import { inferProcedureOutput } from "@trpc/server";
 
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => null,
 });
+
+// infer types from the server
+export type inferQueryResponse<TRouteKey extends keyof AppRouter["_def"]["queries"]> = inferProcedureOutput<
+  AppRouter["_def"]["queries"][TRouteKey]
+>;
